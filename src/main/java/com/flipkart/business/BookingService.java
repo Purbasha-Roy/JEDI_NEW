f3890eaa57670a3e7cb28b8ee4a38cb703a6290e
Purbasha Roy <purbasha2607@gmail.com>
2024-06-30 16:56:55 +0530

    Adding the dropwizard folder

---
+++ src/main/java/com/flipkart/business/BookingService.java
@@ -0,0 +1,11 @@
+package com.flipkart.business;
+
+public class BookingService implements com.flipkart.business.BookingInterface {
+    public void addBooking (String userId, String slotId, String gymId) {
+        return;
+    }
+
+    public void cancelBooking (String bookingId) {
+        return;
+    }
+}
